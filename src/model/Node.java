13a1d499f5a0fac469b036e3a79155ac090f99b3
Manuel Balanta <mbalantaq@gmail.com>
2019-11-04 22:18:01 -0500

    New field and new methods added in class Node

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,4 +1,8 @@
 package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This is the node class for the graph
  * @author A00355202
@@ -11,8 +15,23 @@ public class Node<T> {
 	 * This represent the content of the node
 	 */
 	private T content;
+	private List<T> neighbours;
 	
-	public Node(){
-		
+	public Node(T data){
+		content = data;
+		neighbours = new ArrayList<T>();
 	}
+	
+	public void addNeighbours(T neighbourNode) {
+		this.neighbours.add(neighbourNode);
+	}
+
+	public List<T> getNeighbours() {
+		return neighbours;
+	}
+
+	public void setNeighbours(List<T> neighbours) {
+		this.neighbours = neighbours;
+	}
+
 }
