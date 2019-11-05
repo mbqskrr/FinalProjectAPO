d98c1a32ecd3a4de2a0e80b32c7790740dc0d795
Manuel Balanta <mbalantaq@gmail.com>
2019-11-04 22:40:45 -0500

    BFS method added and finished but not tested. New field in Node class with get and set method

--- src/model/Node.java
+++ src/model/Node.java
@@ -15,13 +15,22 @@ public class Node<T> {
 	 * This represent the content of the node
 	 */
 	private T content;
+	private boolean vistied;
 	private List<T> neighbours;
 	
+	/**
+	 * 
+	 * @param data
+	 */
 	public Node(T data){
 		content = data;
 		neighbours = new ArrayList<T>();
 	}
 	
+	/**
+	 * 
+	 * @param neighbourNode
+	 */
 	public void addNeighbours(T neighbourNode) {
 		this.neighbours.add(neighbourNode);
 	}
@@ -34,4 +43,11 @@ public class Node<T> {
 		this.neighbours = neighbours;
 	}
 
+	public void setVistied(boolean vistied) {
+		this.vistied = vistied;
+	}
+
+	public boolean isVistied() {
+		return vistied;
+	}
 }
--- src/model/graphAlgoritms/BFS.java
+++ src/model/graphAlgoritms/BFS.java
@@ -1,5 +1,44 @@
 package model.graphAlgoritms;
 
-public class BFS {
-
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import model.Node;
+/**
+ * 
+ * @author W-7
+ *
+ * @param <T>
+ */
+public class BFS <T>{
+	private Queue<Node> q;
+	private ArrayList<Node> nodes = new ArrayList<Node>();
+	
+	/**
+	 * 
+	 */
+	public BFS() {
+		q = new LinkedList<Node>();
+	}
+	
+	/**
+	 * 
+	 * @param node
+	 */
+	public void breadthFirstSearch(Node<T> node) {
+		q.offer(node);
+		node.setVistied(true);
+		while (!q.isEmpty()) {
+			Node element = q.poll();
+			List<T> neigbours = element.getNeighbours();
+			for (int i = 0; i < neigbours.size(); i++) {
+				Node n = (Node) neigbours.get(i);
+				if (n != null && !n.isVistied()) {
+					q.offer(n);
+					n.setVistied(true);
+				}
+			}
+		}
+	}
 }
