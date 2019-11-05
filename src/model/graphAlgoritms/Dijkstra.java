ea8c93336b6195dc1b107411933a715a90530881
Manuel Balanta <mbalantaq@gmail.com>
2019-11-04 21:04:12 -0500

    Algorithm Dijkstra finished but not tested

--- src/model/graphAlgoritms/Dijkstra.java
+++ src/model/graphAlgoritms/Dijkstra.java
@@ -1,5 +1,50 @@
 package model.graphAlgoritms;
 
-public class Dijkstra {
+import model.matrixRepresentation.WeightMatrix;
 
+/**
+ * 
+ * @author W-7
+ *
+ * @param <T>
+ */
+public class Dijkstra<T> {
+	private int[][] weights;
+	private int[] last; // último vértice en el camino
+	private int[] cost; // coste mínimo
+	private boolean[] F;
+	private int n; //numero de vertices
+	private int origen;
+	
+	public Dijkstra(WeightMatrix gm, int o) {
+		n = gm.getCapacity();
+		origen = o;
+		last = new int[n];
+		cost = new int[n];
+		F = new boolean[n];
+	}
+	
+	public void minimalPath() {
+		for (int i = 0; i < n; i++) {
+			F[i] = false;
+			cost[i] = weights[origen][i];
+			last[i] = origen;
+		}
+		F[origen] = true;
+		cost[origen] = 0;
+		int v = minimun();
+	}
+
+	private int minimun() {
+		int max = Integer.MAX_VALUE;
+		int v = 1;
+		for (int i = 1; i < n; i++) {
+			if (!F[i] && (cost[i] <= max)) {
+				max = cost[i];
+				v = i;
+			}
+		}
+		return v;
+	}
+	
 }
--- src/model/matrixRepresentation/WeightMatrix.java
+++ src/model/matrixRepresentation/WeightMatrix.java
@@ -1,5 +1,32 @@
 package model.matrixRepresentation;
 
 public class WeightMatrix {
+	private int[][] matrix;
+	private int capacity;
+	
+	public WeightMatrix(int capacity) {
+		this.capacity = capacity;
+		matrix = new int [capacity][capacity];
+	}
+	
+	public WeightMatrix() {
+		
+	}
 
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setMatrix(int[][] matrix) {
+		this.matrix = matrix;
+	}
+
+	public int[][] getMatrix() {
+		return matrix;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
 }
