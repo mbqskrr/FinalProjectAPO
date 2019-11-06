982d33be9b9248a8ad052cfbc3a742cdda5bc8d4
Juan Andres Orozco Nunez <1098819017@academico.icesi.edu.co>
2019-11-06 15:43:29 -0500

    Main has been changed

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,7 +3,6 @@ package ui;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-	}
+		System.out.println("Hola Mundo");	}
 
 }
--- test/model/NodeTest.java
+++ test/model/NodeTest.java
@@ -2,13 +2,17 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class NodeTest {
 
+	@BeforeAll
+	
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		fail();
 	}
 
 }
