d8b01eb8d9ad83423bcb511c3d42b846366ecbf5
Andres Orozco <juanxxi2015@gmail.com>
2019-11-25 17:17:58 -0500

    Paths partially working, just kidding aint work at all

--- src/ui/GraphController.java
+++ src/ui/GraphController.java
@@ -39,6 +39,7 @@ public class GraphController {
     	try {
 			g = new Graph();
 			g.loadCoordenates();
+			g.loadPaths();
 			int n= g.getNodes().size();
 			for (int i = 0; i < n; i++) {
 	    		node = g.getNodes().get(i);
@@ -46,28 +47,9 @@ public class GraphController {
 	    		double y = node.getY();
 	    		t = new Text(x-12, y-12, g.getNodes().get(i).getId());
 	    		c = new Circle(x, y, 10,Color.RED);
-	        	g.loadCoordenates();
-	        	String n1,n2;
-	        	double x1=0,x2=0,y1=0,y2=0;
-	        	int tam = g.getPaths().size();
-	        	for (int i1 = 0; i < tam; i++) {
-	        		System.out.println("Int");
-	    			n1 = g.getPaths().get(i1).getN1();
-	    			n2 = g.getPaths().get(i1).getN2();
-	    			System.out.println(n1+" "+n2);
-	    			if(n1.equals(g.getNodes().get(i1).getId())) {
-	    				 x1 = g.getNodes().get(i1).getX();
-	    				 y1 = g.getNodes().get(i1).getY();
-	    			if((n2.equals(g.getNodes().get(i1).getId()))) {
-	    				 x2 = g.getNodes().get(i1).getX();
-	    				 y2 = g.getNodes().get(i1).getY();
-	    			}
-	    			}
-	    			Line linea = new Line(x1, y1, x2, y2);
-	    			pane.getChildren().add(linea);
-	        	}
 	    		pane.getChildren().add(c);
 	    		pane.getChildren().add(t);
+	    		print();
 	    		
 			}
 			
@@ -80,5 +62,46 @@ public class GraphController {
     	
     }
 
-  
+	private void print() throws IOException {
+    	String n1,n2;
+    	int tam = g.getPaths().size();
+    	for (int i = 0; i < tam; i++) {
+			n1 = g.getPaths().get(i).getN1();
+			n2 = g.getPaths().get(i).getN2();
+			System.out.println(n1+" "+n2);
+			search(n1,n2);
+			}
+    	}
+
+	private void search(String n1, String n2) {
+		Node nd1 = search(n1);
+		Node nd2 = search(n2);
+		if(nd1==null||  nd2==null) {
+			System.out.println("Aqui esta");
+		}else {
+			double x1=0,y1=0,x2=0,y2=0;
+			x1 = nd1.getX();
+			y1 = nd1.getY();
+			x2 = nd2.getX();
+			y2 = nd2.getX();
+			System.out.println("x1: "+x1+"x2: "+x2+"y1: "+y1+"y2: "+y2);
+			Line l = new Line(x1, y1, x2, y2);
+			pane.getChildren().add(l);
+		}
+		
+		
+		
+	}
+
+	private Node search(String dat) {
+		int n = g.getNodes().size();
+		Node n1=null;
+		for (int i = 0; i < n ; i++) {
+			if(dat.equals(g.getNodes().get(i).getId())) {
+				n1=g.getNodes().get(i);
+			}
+		}
+		return n1;
+	}
+		  
 }
